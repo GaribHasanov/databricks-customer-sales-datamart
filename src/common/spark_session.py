477129dc24f71c9104bad51a16681f55557b08a1
GaribHasanov <60735401+GaribHasanov@users.noreply.github.com>
2026-04-26 14:39:52 +0200

    Update spark_session.py

--- src/common/spark_session.py
+++ src/common/spark_session.py
@@ -1,1 +1,10 @@
+from pyspark.sql import SparkSession
 
+def get_spark(app_name: str = "CustomerSalesMedallion"):
+    spark = SparkSession.builder \
+        .appName(app_name) \
+        .config("spark.sql.shuffle.partitions", "200") \
+        .config("spark.sql.adaptive.enabled", "true") \
+        .getOrCreate()
+
+    return spark
