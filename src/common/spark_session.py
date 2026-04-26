f60efc1ad42a35867894027a693b1a49f5fad593
GaribHasanov <60735401+GaribHasanov@users.noreply.github.com>
2026-04-26 14:50:56 +0200

    Refactor get_spark function for SparkSession
    Refactor get_spark function to simplify SparkSession creation.

--- src/common/spark_session.py
+++ src/common/spark_session.py
@@ -1,10 +1,8 @@
 from pyspark.sql import SparkSession
 
-def get_spark(app_name: str = "CustomerSalesMedallion"):
-    spark = SparkSession.builder \
-        .appName(app_name) \
-        .config("spark.sql.shuffle.partitions", "200") \
-        .config("spark.sql.adaptive.enabled", "true") \
+def get_spark(app_name="customer-sales-pipeline"):
+    return (
+        SparkSession.builder
+        .appName(app_name)
         .getOrCreate()
-
-    return spark
+    )
