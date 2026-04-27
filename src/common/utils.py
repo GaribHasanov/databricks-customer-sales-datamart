51f8a24f4cbcb19fd0883f61d3b26312eb697c43
GaribHasanov <60735401+GaribHasanov@users.noreply.github.com>
2026-04-28 00:23:49 +0200

    Add function to normalize DataFrame column names

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,2 +1,5 @@
-for col in df.columns:
-    df = df.withColumnRenamed(col, col.lower())
+from pyspark.sql import functions as F
+
+
+def normalize_column_names(df):
+    return df.toDF(*[c.lower() for c in df.columns])
