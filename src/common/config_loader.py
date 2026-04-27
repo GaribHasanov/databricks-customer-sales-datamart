b383413edbdb0aa5a8d40e30014cb82c1de045a5
GaribHasanov <60735401+GaribHasanov@users.noreply.github.com>
2026-04-28 00:22:23 +0200

    Update config_loader.py

--- src/common/config_loader.py
+++ src/common/config_loader.py
@@ -1,5 +1,18 @@
 import yaml
+import os
 
-def load_config(path: str):
-    with open(path, "r") as file:
+
+def load_config(env: str):
+    base_path = os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(__file__)
+        )
+    )
+
+    config_path = os.path.join(base_path, f"config/{env}.yml")
+
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"Config not found: {config_path}")
+
+    with open(config_path, "r") as file:
         return yaml.safe_load(file)
