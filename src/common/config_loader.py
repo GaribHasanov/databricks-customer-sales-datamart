93d59e0d94b4dce13cfa51d4af5e1ea794d95cbe
GaribHasanov <60735401+GaribHasanov@users.noreply.github.com>
2026-04-28 11:03:21 +0200

    Update config_loader.py

--- src/common/config_loader.py
+++ src/common/config_loader.py
@@ -1,18 +1,16 @@
 import yaml
 import os
 
-
 def load_config(env: str):
-    base_path = os.path.dirname(
-        os.path.dirname(
-            os.path.dirname(__file__)
-        )
-    )
-
-    config_path = os.path.join(base_path, f"config/{env}.yml")
-
+    # Bu funksiya avtomatik olaraq layihənin kökünü tapıb config-i oxuyacaq
+    # /Workspace/Repos/.../project/src/common -> /Workspace/Repos/.../project/
+    current_path = os.path.abspath(__file__)
+    project_root = os.path.dirname(os.path.dirname(os.path.dirname(current_path)))
+    
+    config_path = f"{project_root}/config/{env}.yml"
+    
     if not os.path.exists(config_path):
-        raise FileNotFoundError(f"Config not found: {config_path}")
-
+        raise FileNotFoundError(f"Config tapılmadı: {config_path}")
+        
     with open(config_path, "r") as file:
         return yaml.safe_load(file)
