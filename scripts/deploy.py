5c2f8c390bd258fa92a757505dfd557fe78fc60b
GaribHasanov <60735401+GaribHasanov@users.noreply.github.com>
2026-05-02 13:26:31 +0200

    Update deploy.py

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,4 +1,5 @@
 import os
+import base64
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.workspace import ImportFormat
 
@@ -7,13 +8,14 @@ w = WorkspaceClient(
     token=os.environ["DATABRICKS_TOKEN"]
 )
 
+# file oxu və base64 et
 with open("notebooks/01_bronze_customer_ingestion.py", "rb") as f:
-    content = f.read()
+    content = base64.b64encode(f.read()).decode("utf-8")
 
 w.workspace.import_(
     path="/Users/bronze/01_bronze_customer_ingestion",
     format=ImportFormat.SOURCE,
-    content=content,
+    content=content,   # artıq string-dir
     overwrite=True
 )
 
