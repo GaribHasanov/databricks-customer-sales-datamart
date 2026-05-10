2e6dce71522ce117a7611f5643689d1c306c4bfd
hasanov.garib@gmail.com <hasanov.garib@gmail.com>
2026-05-10 19:03:57 +0000

    Split single cell into multiple cells

--- notebooks/01_bronze_customer_ingestion.py
+++ notebooks/01_bronze_customer_ingestion.py
@@ -5,13 +5,13 @@
 # ///
 from pyspark.sql.functions import current_timestamp
 
-# ---------------------------
-# BUNDLE CONFIG (runtime parameters)
-# ---------------------------
+# COMMAND ----------
 
 dbutils.widgets.text("catalog", "dev")
 dbutils.widgets.text("schema", "bronze")
 dbutils.widgets.text("customer_volume_path", "/Volumes/dev/00_landing_zone/01_managed_volume")
+
+# COMMAND ----------
 
 catalog = dbutils.widgets.get("catalog")
 schema = dbutils.widgets.get("schema")
@@ -19,30 +19,27 @@ customer_volume_path = dbutils.widgets.get("customer_volume_path")
 
 table_name = f"{catalog}.{schema}.customer_bronze"
 
-# ---------------------------
-# READ RAW DATA
-# ---------------------------
+# COMMAND ----------
+
 df = spark.read.format("csv") \
     .option("header", True) \
     .option("inferSchema", True) \
     .load(customer_volume_path)
 
-# ---------------------------
-# BRONZE ENRICHMENT
-# ---------------------------
+# COMMAND ----------
+
 df = df.withColumn("ingestion_time", current_timestamp())
 
-# ---------------------------
-# WRITE TO DELTA BRONZE
-# ---------------------------
+# COMMAND ----------
+
 spark.sql(f"CREATE SCHEMA IF NOT EXISTS {catalog}.{schema}")
+
+# COMMAND ----------
 
 df.write.format("delta") \
     .mode("append") \
     .saveAsTable(table_name)
 
-print(f"Loaded data into {table_name}")
-
 # COMMAND ----------
 
-
+print(f"Loaded data into {table_name}")
